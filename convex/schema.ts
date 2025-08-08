fab57957d18c8bef8e66cb201bbfec465df2c82d
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-08 13:00:41 +0500

    added prettier

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,5 +1,5 @@
-import { defineSchema, defineTable } from "convex/server";
+import { defineSchema, defineTable } from 'convex/server';
 
-import { v } from "convex/values";
+import { v } from 'convex/values';
 
 export default defineSchema({});
