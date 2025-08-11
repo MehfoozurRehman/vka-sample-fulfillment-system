6b55f0305c7225a91e32aed75ff858fad6c15536
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-11 18:26:12 +0500

    Update page.tsx

--- app/(protected)/admin/products/page.tsx
+++ app/(protected)/admin/products/page.tsx
@@ -12,9 +12,7 @@ export default function ProductsPage() {
   return (
     <div className="@container/main flex flex-1 flex-col gap-2">
       <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-        <div className="px-4 lg:px-6">
-          <Stats data={data || []} />
-        </div>
+        <Stats data={data || []} />
         <div className="px-4 lg:px-6">
           <Chart data={data || []} />
         </div>
