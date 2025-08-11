074f5477705ffecf434232c529d44c1be0b4cb4c
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-11 16:04:47 +0500

    Create invite-user.tsx

---
+++ app/(protected)/admin/users/components/invite-user.tsx
@@ -0,0 +1,113 @@
+'use client';
+
+import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { useState, useTransition } from 'react';
+
+import { Button } from '@/components/ui/button';
+import { IconPlus } from '@tabler/icons-react';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Loader } from 'lucide-react';
+import { api } from '@/convex/_generated/api';
+import { roles } from '@/constants';
+import { toast } from 'sonner';
+import toastError from '@/utils/toastError';
+import { useMutation } from 'convex/react';
+
+export function InviteUser() {
+  const [open, setOpen] = useState(false);
+
+  const inviteUser = useMutation(api.user.inviteUser);
+
+  const [isPending, startTransition] = useTransition();
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    startTransition(async () => {
+      const formData = new FormData(event.currentTarget);
+      const name = formData.get('name') as string;
+      const email = formData.get('email') as string;
+      const role = formData.get('role') as string;
+
+      if (!name) {
+        toast.error('Name is required');
+        return;
+      }
+
+      if (!email) {
+        toast.error('Email is required');
+        return;
+      }
+
+      if (!role) {
+        toast.error('Role is required');
+        return;
+      }
+
+      try {
+        await inviteUser({ name, email, role });
+
+        setOpen(false);
+
+        toast.success('User invited successfully');
+      } catch (error) {
+        toastError(error);
+      }
+    });
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button variant="default" size="sm">
+          <IconPlus />
+          <span className="hidden lg:inline">Invite User</span>
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[500px]">
+        <form onSubmit={handleSubmit}>
+          <DialogHeader className="mb-4">
+            <DialogTitle>Invite User</DialogTitle>
+            <DialogDescription>Invite a new user to the platform. Please provide their name, email, and role. The user will receive an invitation email to join.</DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4">
+            <div className="grid gap-3">
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" name="name" />
+            </div>
+            <div className="grid gap-3">
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" name="email" />
+            </div>
+            <div className="grid gap-3">
+              <Label htmlFor="role">Role</Label>
+              <Select name="role">
+                <SelectTrigger className="flex w-full **:data-[slot=select-value]:block **:data-[slot=select-value]:truncate @[767px]/card:hidden capitalize" size="sm" aria-label="Select a role">
+                  <SelectValue placeholder="Select a role" />
+                </SelectTrigger>
+                <SelectContent className="rounded-xl">
+                  {roles.map((role) => (
+                    <SelectItem key={role} value={role} className="rounded-lg capitalize">
+                      {role}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
+          <DialogFooter className="mt-4">
+            <DialogClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DialogClose>
+            <Button type="submit" disabled={isPending}>
+              {isPending && <Loader className="mr-2 size-4 animate-spin" />}
+              Invite
+            </Button>
+          </DialogFooter>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+}
