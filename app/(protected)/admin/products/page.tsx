d4d6e0f983140e65f31f902361d3ae249d54fe8f
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-11 18:21:33 +0500

    Update page.tsx

--- app/(protected)/admin/products/page.tsx
+++ app/(protected)/admin/products/page.tsx
@@ -1,3 +1,25 @@
+'use client';
+
+import { Chart } from './components/chart';
+import { DataTable } from './components/table';
+import { Stats } from './components/stats';
+import { api } from '@/convex/_generated/api';
+import { useQueryWithStatus } from '@/hooks/use-query';
+
 export default function ProductsPage() {
-  return <div>ProductsPage</div>;
+  const { data, isPending } = useQueryWithStatus(api.product.list);
+
+  return (
+    <div className="@container/main flex flex-1 flex-col gap-2">
+      <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+        <div className="px-4 lg:px-6">
+          <Stats data={data || []} />
+        </div>
+        <div className="px-4 lg:px-6">
+          <Chart data={data || []} />
+        </div>
+        <DataTable data={data || []} isPending={isPending} />
+      </div>
+    </div>
+  );
 }
