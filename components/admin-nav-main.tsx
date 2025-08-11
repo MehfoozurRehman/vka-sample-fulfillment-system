3fb4fea1e92d2369c8ed4f782d683b728b108f79
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-11 10:30:49 +0500

    Update admin-nav-main.tsx

--- components/admin-nav-main.tsx
+++ components/admin-nav-main.tsx
@@ -5,15 +5,7 @@ import { type Icon } from '@tabler/icons-react';
 
 import { SidebarGroup, SidebarGroupContent, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 
-export function NavMain({
-  items,
-}: {
-  items: {
-    title: string;
-    url: string;
-    icon?: Icon;
-  }[];
-}) {
+export function NavMain({ items }: { items: { title: string; url: string; icon?: Icon }[] }) {
   return (
     <SidebarGroup>
       <SidebarGroupContent className="flex flex-col gap-2">
