12f52a80ebcf71e07201a8e33bd2f4c2e828cbc4
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-11 14:33:57 +0500

    Create layout.tsx

---
+++ app/(protected)/layout.tsx
@@ -0,0 +1,26 @@
+import { cookies, headers } from 'next/headers';
+
+import AuthProvider from '@/providers/auth';
+import React from 'react';
+import { TOKEN_COOKIE_NAME } from '@/constants';
+import { api } from '@/convex/_generated/api';
+import { fetchQuery } from 'convex/nextjs';
+import { redirect } from 'next/navigation';
+
+export default async function ProtectedLayout({ children }: { children: React.ReactNode }) {
+  const cookie = await cookies();
+
+  const token = cookie.get(TOKEN_COOKIE_NAME)?.value || null;
+
+  if (!token) {
+    return redirect('/');
+  }
+
+  const user = await fetchQuery(api.auth.getUser, { userId: token });
+
+  if (!user) {
+    return redirect('/');
+  }
+
+  return <AuthProvider user={user}>{children}</AuthProvider>;
+}
