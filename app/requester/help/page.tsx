31b7570fc8e309920b9893c9d019afe056095797
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-11 14:35:12 +0500

    Delete page.tsx

--- app/requester/help/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function HelpPage() {
-  return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Help Page</h1>
-      <p>This is the help page content. Here you can provide assistance and information to users.</p>
-    </div>
-  );
-}
