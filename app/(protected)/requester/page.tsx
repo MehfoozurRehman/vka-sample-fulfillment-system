11021ea8e1d243e936f4638b26b1e1d0c5add5ed
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-12 16:24:12 +0500

    Update page.tsx

--- app/(protected)/requester/page.tsx
+++ app/(protected)/requester/page.tsx
@@ -1,3 +1,29 @@
+'use client';
+
+import { AddRequest } from './components/add-request';
+import { Chart } from './components/chart';
+import { DataTable } from './components/table';
+import { Stats } from './components/stats';
+import { api } from '@/convex/_generated/api';
+import { useAuth } from '@/hooks/use-user';
+import { useQueryWithStatus } from '@/hooks/use-query';
+
 export default function RequesterPage() {
-  return <div>RequesterPage</div>;
+  const { data } = useQueryWithStatus(api.request.recent, { limit: 200 });
+  const user = useAuth();
+
+  return (
+    <div className="@container/main flex flex-1 flex-col gap-2">
+      <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+        <Stats data={data || []} />
+        <div className="px-4 flex items-center justify-end lg:px-6">
+          <AddRequest requesterEmail={user.email} />
+        </div>
+        <div className="px-4 lg:px-6">
+          <Chart data={data || []} />
+        </div>
+        <DataTable data={data || []} />
+      </div>
+    </div>
+  );
 }
