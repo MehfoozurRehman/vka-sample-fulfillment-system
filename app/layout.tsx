f5ce07d8f6571683502a64597cc7bded3e1805d2
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-12 17:00:30 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import './globals.css';
 import { ConvexClientProvider } from '@/providers/convex';
 import type { Metadata } from 'next';
 import { Nunito } from 'next/font/google';
+import { NuqsAdapter } from 'nuqs/adapters/next/app';
+import { Suspense } from 'react';
 import { ThemeProvider } from '@/providers/theme';
 import { Toaster } from '@/components/ui/sonner';
 
@@ -24,12 +26,14 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${nunito.variable} antialiased`}>
+        {/* <Suspense fallback={<div className="flex items-center justify-center h-screen">Loading...</div>}> */}
         <ConvexClientProvider>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-            {children}
+            <NuqsAdapter>{children}</NuqsAdapter>
             <Toaster />
           </ThemeProvider>
         </ConvexClientProvider>
+        {/* </Suspense> */}
       </body>
     </html>
   );
