438f4e2448c18448647bbaa9417f669e5fd6dd9f
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-12 10:11:42 +0500

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -1,3 +1,3 @@
 export const roles = ['admin', 'requester', 'screener', 'packer', 'shipper'] as const;
-export type Roles = (typeof roles)[number];
+export type RoleType = (typeof roles)[number];
 export const TOKEN_COOKIE_NAME = 'vka_token';
