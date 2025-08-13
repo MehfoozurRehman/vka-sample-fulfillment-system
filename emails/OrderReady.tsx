eb92d3d0b77e2cc762a51dc9311921406cfb50b8
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-14 01:47:31 +0500

    Create OrderReady.tsx

---
+++ emails/OrderReady.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+
+import VkaLayout from './VkaLayout';
+import { render } from '@react-email/render';
+
+export interface OrderReadyProps {
+  requestId: string;
+  orderId: string;
+}
+
+export function OrderReady({ requestId, orderId }: OrderReadyProps) {
+  const title = `VKA Order [${orderId}] Ready to Pack`;
+  const body = `Hello,
+
+Order ${orderId} from request ${requestId} is ready for packing.
+
+Thank you,
+VKA`;
+  return <VkaLayout title={title} body={body} />;
+}
+
+export function renderOrderReadyHtml(props: OrderReadyProps) {
+  return render(<OrderReady {...props} />);
+}
+
+export default OrderReady;
