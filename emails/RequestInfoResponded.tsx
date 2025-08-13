e467f6170f8234981b411ffed8e6707da20569dd
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-14 01:47:42 +0500

    Create RequestInfoResponded.tsx

---
+++ emails/RequestInfoResponded.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+
+import VkaLayout from './VkaLayout';
+import { render } from '@react-email/render';
+
+export interface RequestInfoRespondedProps {
+  requestId: string;
+  message: string;
+}
+
+export function RequestInfoResponded({ requestId, message }: RequestInfoRespondedProps) {
+  const title = `VKA Sample Request [${requestId}] – Information Provided`;
+  const body = `Hello,
+
+The requester has provided additional information for ${requestId}:
+${message}
+
+Thank you,
+VKA`;
+  return <VkaLayout title={title} body={body} />;
+}
+
+export function renderRequestInfoRespondedHtml(props: RequestInfoRespondedProps) {
+  return render(<RequestInfoResponded {...props} />);
+}
+
+export default RequestInfoResponded;
