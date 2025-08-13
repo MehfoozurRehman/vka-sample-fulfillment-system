d124698e55e780e3178a30f81c27d9ccff537da6
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-14 01:47:35 +0500

    Create RequestApproved.tsx

---
+++ emails/RequestApproved.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+
+import VkaLayout from './VkaLayout';
+import { render } from '@react-email/render';
+
+export interface RequestApprovedProps {
+  requestId: string;
+}
+
+export function RequestApproved({ requestId }: RequestApprovedProps) {
+  const title = `VKA Sample Request [${requestId}] Approved`;
+  const body = `Hello,
+
+Your sample request ${requestId} has been approved and is moving to packing.
+
+Thank you,
+VKA`;
+  return <VkaLayout title={title} body={body} />;
+}
+
+export function renderRequestApprovedHtml(props: RequestApprovedProps) {
+  return render(<RequestApproved {...props} />);
+}
+
+export default RequestApproved;
