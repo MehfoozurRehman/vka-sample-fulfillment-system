c87f3be72d9094635a3643214ee677f38c825a5a
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-13 17:21:31 +0500

    Update drawer.tsx

--- components/ui/drawer.tsx
+++ components/ui/drawer.tsx
@@ -41,7 +41,6 @@ function DrawerContent({ className, children, ...props }: React.ComponentProps<t
           'group/drawer-content bg-background fixed z-50 flex h-auto flex-col',
           'data-[vaul-drawer-direction=top]:inset-x-0 data-[vaul-drawer-direction=top]:top-0 data-[vaul-drawer-direction=top]:mb-24 data-[vaul-drawer-direction=top]:max-h-[80vh] data-[vaul-drawer-direction=top]:rounded-b-lg data-[vaul-drawer-direction=top]:border-b',
           'data-[vaul-drawer-direction=bottom]:inset-x-0 data-[vaul-drawer-direction=bottom]:bottom-0 data-[vaul-drawer-direction=bottom]:mt-24 data-[vaul-drawer-direction=bottom]:max-h-[80vh] data-[vaul-drawer-direction=bottom]:rounded-t-lg data-[vaul-drawer-direction=bottom]:border-t',
-          // Updated left/right widths: 75% default (mobile), 50% on sm+, 25% on lg+
           'data-[vaul-drawer-direction=right]:inset-y-0 data-[vaul-drawer-direction=right]:right-0 data-[vaul-drawer-direction=right]:w-3/4 sm:data-[vaul-drawer-direction=right]:w-1/2 lg:data-[vaul-drawer-direction=right]:w-1/4 data-[vaul-drawer-direction=right]:border-l',
           'data-[vaul-drawer-direction=left]:inset-y-0 data-[vaul-drawer-direction=left]:left-0 data-[vaul-drawer-direction=left]:w-3/4 sm:data-[vaul-drawer-direction=left]:w-1/2 lg:data-[vaul-drawer-direction=left]:w-1/4 data-[vaul-drawer-direction=left]:border-r',
           className,
