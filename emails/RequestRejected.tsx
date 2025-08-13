49b0f10fbf12676dd85b59e35c7464b3f7f79295
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-14 01:47:46 +0500

    Create RequestRejected.tsx

---
+++ emails/RequestRejected.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+
+import VkaLayout from './VkaLayout';
+import { render } from '@react-email/render';
+
+export interface RequestRejectedProps {
+  requestId: string;
+  reason: string;
+}
+
+export function RequestRejected({ requestId, reason }: RequestRejectedProps) {
+  const title = `VKA Sample Request [${requestId}] Status Update`;
+  const body = `Hello,
+
+We are sorry to inform you that request ${requestId} was rejected.
+Reason: ${reason}
+
+Thank you,
+VKA`;
+  return <VkaLayout title={title} body={body} />;
+}
+
+export function renderRequestRejectedHtml(props: RequestRejectedProps) {
+  return render(<RequestRejected {...props} />);
+}
+
+export default RequestRejected;
