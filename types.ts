75c2dcaad229908931dae63a42ddf38486d75b77
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-13 19:25:29 +0500

    Update types.ts

--- types.ts
+++ types.ts
@@ -1,17 +1,4 @@
 import { useQuery } from 'convex/react';
 import { api } from './convex/_generated/api';
 
-export interface GoogleLoginResponse {
-  sub: string;
-  name: string;
-  given_name: string;
-  family_name: string;
-  picture: string;
-  email: string;
-  email_verified: boolean;
-}
-
-export type User = NonNullable<ReturnType<typeof useQuery<typeof api.auth.getUser>>> & {
-  roles?: string[];
-  activeRole?: string;
-};
+export type User = NonNullable<ReturnType<typeof useQuery<typeof api.auth.getUser>>> & { roles?: string[]; activeRole?: string };
