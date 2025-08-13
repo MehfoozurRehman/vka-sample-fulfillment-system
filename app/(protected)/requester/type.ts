fdf59623f0275e2ff3a326b06adb2b4fd1b832c4
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-13 22:06:49 +0500

    Update type.ts

--- app/(protected)/requester/type.ts
+++ app/(protected)/requester/type.ts
@@ -1,4 +1,4 @@
 import { api } from '@/convex/_generated/api';
 import { useQuery } from 'convex/react';
 
-export type RecentRequestsType = NonNullable<ReturnType<typeof useQuery<typeof api.request.recent>>>;
+export type RecentRequestsType = NonNullable<ReturnType<typeof useQuery<typeof api.request.my>>>;
