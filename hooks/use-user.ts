7c7a9946617fccd1ddd81b95e3d1037547d886ae
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-15 00:21:49 +0500

    Update use-user.ts

--- hooks/use-user.ts
+++ hooks/use-user.ts
@@ -1,12 +1,7 @@
 import { AuthContext } from '@/providers/auth';
-import { User } from '@/types';
+import { AuthUser } from '@/types';
 import { useContext } from 'react';
 
 export function useAuth() {
-  const context = useContext(AuthContext) as User;
-
-  if (!context) {
-    throw new Error('useAuth must be used within an AuthProvider');
-  }
-  return context;
+  return useContext(AuthContext) as AuthUser;
 }
