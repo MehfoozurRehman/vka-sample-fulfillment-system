2e9556d9b9b205dc708da21e1e3e3f6d2c2ea561
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-15 00:26:24 +0500

    Update RequestApproved.tsx

--- emails/RequestApproved.tsx
+++ emails/RequestApproved.tsx
@@ -1,7 +1,7 @@
 import VkaLayout from './VkaLayout';
 import { render } from '@react-email/render';
 
-export type RequestApprovedProps = {
+type RequestApprovedProps = {
   requestId: string;
 };
 
