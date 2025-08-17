7c95f9490efbc4c0a119af78c9c14a33d051a72f
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-17 11:35:49 +0500

    Update stats.tsx

--- app/(protected)/admin/products/components/stats.tsx
+++ app/(protected)/admin/products/components/stats.tsx
@@ -9,8 +9,6 @@ export function Stats(props: { data: ProductType[] } & React.HTMLAttributes<HTML
 
   const categories = new Set(props.data?.map((p) => p.category)).size;
 
-  const locations = new Set(props.data?.map((p) => p.location)).size;
-
   return (
     <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
       <Card className="@container/card">
@@ -25,12 +23,7 @@ export function Stats(props: { data: ProductType[] } & React.HTMLAttributes<HTML
           <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">{categories.toLocaleString()}</CardTitle>
         </CardHeader>
       </Card>
-      <Card className="@container/card">
-        <CardHeader>
-          <CardDescription>Locations</CardDescription>
-          <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">{locations.toLocaleString()}</CardTitle>
-        </CardHeader>
-      </Card>
+      {/* Locations removed */}
     </div>
   );
 }
